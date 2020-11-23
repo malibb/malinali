97c4cffd6a1aa1005a8eaa60a8b7f97592cc3d05
Malinali Becerril <mali@devf.mx>
2020-11-23 01:52:55 -0600

    add index

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import Layout from './Components/Layout';
+import './App.sass';
+
+const style = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center"
+};
 
 function App() {
-  return (
-    <Layout>
-      <h1>¡Hi Everyone!</h1>
-    </Layout>
-  );
+  return (<div className="container">
+    <section className="hero is-primary is-bold" style={{ height: "100vh" }}>
+      <div className="hero-body" style={style}>
+          <h1 className="title is-1 is-spaced" style={{textAlign:"center"}}>
+            Malinali Becerril Bernal
+          </h1>
+          <h2 className="subtitle ">
+            Software Engineer
+          </h2>
+      </div>
+    </section>
+  </div>);
 }
 
 export default App;
