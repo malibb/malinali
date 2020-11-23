e1d5f73b4f913fd10f938ee77e1dc8f47c06e9a9
Malinali Becerril <mali@devf.mx>
2020-11-23 01:59:35 -0600

    create navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,22 @@
 
 import React from 'react';
+import logo from './../logo.svg'
 
 const Navbar = ({message}) => {
     
-    return <header>
-        <h1>{message}</h1>
-    </header>;
+    return <nav className="navbar primary" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <a className="navbar-item" href="/">
+        <img src={logo} alt="Malinali B"/>
+      </a>
+  
+      <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+  </nav>;
 };
 
 export default Navbar;
